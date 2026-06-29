3b94bddc840766f26db2d007d85145ccd54c7b61
SilkyFinish <3259731417@qq.com>
2026-06-29 13:41:10 +0800

    homing fix

--- collect/homing.py
+++ collect/homing.py
@@ -1,21 +1,44 @@
-'''
-usage: python homing.py -id <1 or 2>
-'''
-from r3kit.devices.robot.flexiv.rizon import Rizon
 import argparse
+
+
+"""
+Usage:
+    python homing.py -id <1 or 2>
+"""
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Choose which robot to home",
     )
-    parser.add_argument("id", type=int, required=True, help="1 refers to master robot, 2 refers to slave robot")
+    parser.add_argument(
+        "-id",
+        "--id",
+        dest="robot_id",
+        type=int,
+        choices=[1, 2],
+        required=True,
+        help="1 refers to master robot, 2 refers to slave robot",
+    )
+    return parser.parse_args()
+
 
 def main():
+    from r3kit.devices.robot.flexiv.rizon import Rizon
+
     args = parse_args()
-    if args.id == 1:
-        robot = Rizon(id='Rizon4s-063652', gripper=False, name='Rizon4s')
-    elif args.id == 2:
-        robot = Rizon(id='Rizon4s-063586', gripper=False, name='Rizon4s')
+    if args.robot_id == 1:
+        robot_sn = "Rizon4s-063652"
+    elif args.robot_id == 2:
+        robot_sn = "Rizon4s-063586"
     else:
         raise ValueError("Invalid robot ID")
+
+    print(f"Homing robot {args.robot_id}: {robot_sn}")
+    robot = Rizon(id=robot_sn, gripper=False, name="Rizon4s")
     robot.homing()
+    print("Homing command finished")
+
+
+if __name__ == "__main__":
+    main()
